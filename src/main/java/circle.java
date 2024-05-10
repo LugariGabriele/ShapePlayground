2b8e1700f3ed0f790a8928d5af5c64cab613154e
LugariGabriele <161489816+LugariGabriele@users.noreply.github.com>
2024-05-10 15:06:04 +0200

    change box and adjust circle's class

--- src/main/java/circle.java
+++ src/main/java/circle.java
@@ -1,11 +1,21 @@
 import org.dyn4j.exception.ValueOutOfRangeException;
+import java.awt.geom.Dimension2D;
+import java.awt.*;
 
 public class circle {
     double radius;
+    Color color;
 
-    public circle(double radius) {
+    public circle(double radius, Color color) {
         this.radius = radius;
+        this.color = color;
     }
+
+    /**
+     *
+     * @param radius
+     * @return a warning string if the radius is negative
+     */
     private static boolean validate(double radius) {
         if (radius <= 0.0) {
             throw new ValueOutOfRangeException("radius", radius, "greater than", 0.0);
@@ -13,12 +23,23 @@ public class circle {
             return true;
         }
     }
-    public double circonference(){
+
+    /**
+     *
+     * @return the circonference of the circle
+     */
+    public double getCirconference(){
         return 2*this.radius*Math.PI;
     }
+
+    /**
+     *
+     * @return the area of the circle
+     */
     public double getArea() {
 
         return Math.PI * this.radius * this.radius;
     }
 }
+
 
